3aacc8c7a4bd7ee159b95bad96fdd08b0beaeec9
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-01-30 20:52:25 +0200

    home page progress

--- client/src/components/HomeFolder/MainHome.js
+++ client/src/components/HomeFolder/MainHome.js
@@ -1,12 +1,38 @@
 import React, { Fragment, useState } from "react";
-
+import "../../App.css";
 const MainHome = () =>{
+    const clickedAmount= 0
     return(
         <Fragment>
-            <div className="container">
-                <div>
-                    <h1>HomePage</h1>
+            <div className=" bg-black h-screen">
+                <h1 class="font-mono text-white text-4xl text-center">My Website</h1>
+                <p class="animate-wiggle font-mono text-white mb-5 text-2xl max-w-screen-lg" >this is a collection of several pages</p>
+                <div class="mt-6 ml-2 flex flex-row">
+                    <div class="rounded-3xl bg-gray-900 w-1/4 p-6 items-center shadow-xl animate-fade-in-down flex-shrink max-w-screen-lg">
+                        <div class="flex justify-center items-center space-x-3 mt-3 mb-3">
+                            <img src="https://img.icons8.com/ios-filled/100/000000/inspection.png"/>
+                            <h3 class="text-3xl text-white font-mono shadow-2xl">Todo application</h3>
+                        </div>
+                        <hr></hr>
+                        <div class="flex justify-center items-center space-x-3 mt-3 mb-3">
+                            <img src="https://img.icons8.com/ios/100/000000/apple-camera.png"/>
+                            <h3 class="text-3xl text-white font-mono shadow-2xl">Photo collection</h3>
+                        </div>
+                    </div>
+                    <div class="w-1/2">
+
+                    </div>
+                    <div class="mx-20 mt-20">
+                        <h3 class="text-white text-sm font-mono">This button has been clicked {clickedAmount} times</h3>
+                        <button class="btn btn-danger w-fit">
+                            Click me
+                        </button>
+                    </div>
                 </div>
+
+
+
+
             </div>
         </Fragment>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,34 @@ module.exports = {
     extend: {
       backgroundImage:{
         'todobg':"url('/src/assets/todobg.jpg')",
+      },
+      keyframes: {
+        wiggle: {
+            '0%, 100%': {
+                transform: 'rotate(-2deg)'
+            },
+            '50%': {
+                transform: 'rotate(2deg)'
+            },
+        },
+        'fade-in-down': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateY(-10px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
       }
+      
     },
   },
+  variants: {},
   plugins: [],
 }
