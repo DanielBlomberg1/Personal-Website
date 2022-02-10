50c4667668bcfdcbd16c1dd7d42ce664dc99f095
DanielBlomberg1 <daniel.blombergof@gmail.com>
2022-02-10 10:10:27 +0200

    made calculator functional now correctly calculates addition

--- client/src/components/Calculator/CalcMain.js
+++ client/src/components/Calculator/CalcMain.js
@@ -3,17 +3,35 @@ import {Link} from 'react-router-dom';
 import "../../App.css";
 
 const CalcMain = () =>{
+    
+
     const operatorList = ['+','-', '/', '*'];
+    /*
     const operatorfuncs = {
         '+': function(a, b) {return a+b},
         '-': function(a, b) {return a-b},
         '*': function(a, b) {return a*b},
         '/': function(a, b) {return a/b},
     }
+    */
     const specialChars = new Map;
     specialChars.set('π', 3.14);
     specialChars.set('e', 2.7);
     
+
+    function splitAtOperator(string){
+        var smallest = 9999;
+        operatorList.forEach(e=> {
+            var index = string.indexOf(e);
+            if(index != -1 && index < smallest){
+                smallest = index;
+            }
+        });
+        string = string.substring(0, smallest);
+
+        return string;
+    }
+
 
     function isNumber(char) {
         if (typeof char !== 'string') {
@@ -28,90 +46,26 @@ const CalcMain = () =>{
       }
 
 
-    function getNumberNearOperator(s, boolean){ 
-        console.log(s);
-        var evaluate;
-        if(s == null){return;}
-        let smallest = 9999;
-
-        operatorList.forEach(e => {
-            let eI = s.lastIndexOf(e);
-            const indeces = [...s.matchAll(new RegExp("["+e+"]", 'gi'))].map(a => a.index);
-            console.log(indeces);
-            //console.log("index of " + e + " is "+ eI);
-
-            indeces.forEach(i =>{
-                var part1 = s.substring(0, i);
-                var part2 = s.substring(i);
-
-                for(var i = 0; i < part2.length; i++){
-                    if(!isNumber(part2.charAt(i))){
-                        part2 = part2.substring(0, i);
-                        i=part2.length;
-                    }   
-                }
-                console.log("part1 : " + part1 + " part2: " + part2);
-                evaluate +=operatorfuncs[e](part1, part2);
-
-            });
-
-            if(eI < smallest && eI != -1){
-                smallest = eI;
-            }
-        });
-        
-        console.log("evaluate =  "+ evaluate);
-        return s;
-    }
-
-    function evaluatePart(s){
-        //console.log(s);
-        let multipli = s.indexOf('*');
-        let s1 = s.substring(0, multipli);
-        let s2 = s.substring(multipli + 1);
-        
-        if(s1.indexOf('/') != -1){
-            console.log("/ not implemented yet");
-            return;
-        }
-
-        s1 = getNumberNearOperator(s1, true);
-        s2 = getNumberNearOperator(s2, false);
-        //console.log("parts = " + s1 + "  "+s2);
-        let evaluation = s1 * s2;
-
-        return evaluation;
-    }
-
-
     function handleChange(){
-        var text1 = document.getElementById('input-main').value; 
-        var raturnval=0;
+        let text1 = document.getElementById('input-main').value; 
         text1 = text1.replace(/\s+/g, '');
 
-        if(text1.indexOf('(') != -1){
-            if(text1.indexOf(')') != -1 ||  text1.indexOf(')') > text1.indexOf('(')){
-                let partresult = text1.substring(text1.indexOf('(') + 1, text1.indexOf(')'));
-                //console.log(text1);
-                let oldPartResult = partresult;
-
-                partresult = evaluatePart(partresult);
-                text1.replace(oldPartResult, partresult);
-
-                console.log(text1 + "  evaluation " + partresult + " old part result " + oldPartResult);
-            }
-        }
-
-        operatorList.forEach(e => {
-            const indeces = [...text1.matchAll(new RegExp("["+e+"]", 'gi'))].map(a => a.index);
-            indeces.forEach(i =>{
-                console.log(text1);
-                console.log(indeces);
-                let n1 = getNumberNearOperator(text1, true);
-                let n2 = getNumberNearOperator(text1, false);
-            });
-        });
+        let indeces = [...text1.matchAll(new RegExp("[+]", 'gi'))].map(a => a.index);
         
+        while (indeces.length != 0 && indeces[0]+1 != text1.length && indeces[0] != "+"){
+
+            let part1 = text1.substring(0, indeces[0]);
+            let part2 = text1.substring(indeces[0]+1);
+            part2 = splitAtOperator(part2);
+            let together =part1+"+"+part2;
+            if(part1.length > 0 && part2.length > 0){
+                //var eval = part1 + part2;
+                text1 = text1.replace(together, (parseFloat(part1)+parseFloat(part2)));
+                //console.log("result = " + text1 + " part 1 = " + part1+" part2 = "+ part2);
+            }
+            indeces = indeces = [...text1.matchAll(new RegExp("[+]", 'gi'))].map(a => a.index);
+        }
+        var res = document.getElementById('result').innerHTML = text1;
     };
 
     return(
@@ -125,10 +79,14 @@ const CalcMain = () =>{
                     </Link>
                 </div>
             </div>
-            <div className="ml-3">
+            <div className="ml-5">
                 <div>
                     <h3 className="text-3xl text-slate-300 font-mono" >Input here:</h3>
-                    <textarea id="input-main" onChange={()=>handleChange()} placeholder="input mathematical expression..." className="border-3 w-2/6 h-1/6 bg-slate-700 text-violet-100 border-slate-500 p-2 align-text-top"></textarea>
+                    <div className="flex flex-row">
+                        <textarea id="input-main" onChange={()=>handleChange()} placeholder="input mathematical expression..." className="border-3 w-2/6 h-1/6 bg-slate-700 text-violet-100 border-slate-500 p-2 align-text-top"></textarea>
+                        <p  className="text-white font-mono text-3xl p-4 text-center">Evaluation = <span id="result"></span></p>
+                    </div>
+
                 </div>
                 <div className="grid gap-4 grid-cols-6 grid-rows-6 w-2/6 h-1/6 aspect-square m-12">
                     <button className="text-4xl rounded-xl pt-1 mt-2 bg-slate-500">
