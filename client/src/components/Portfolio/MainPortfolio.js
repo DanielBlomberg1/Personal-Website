85767c62a2626a2dc21ece9a6fa8220789040c48
DanielBlomberg1 <daniel.blombergof@gmail.com>
2022-02-11 13:34:09 +0200

    make app-portoifolio-body

--- client/src/components/Portfolio/MainPortfolio.js
+++ client/src/components/Portfolio/MainPortfolio.js
@@ -1,10 +1,13 @@
 import React,{Fragment} from "react";
 import PortfolioHeader from "./PortfolioHeader"
+import PortfolioBody from "./PortfolioBody";
+
 import '../../index.css';
 const Portfolio = () =>{
     return(
         <Fragment>
             <PortfolioHeader/>
+            <PortfolioBody/>
         </Fragment>
     )
 }
---
+++ client/src/components/Portfolio/PortfolioBody.js
@@ -0,0 +1,10 @@
+import React,{Fragment} from "react";
+
+const PortfolioBody = () =>{
+    return(
+        <Fragment>
+
+        </Fragment>
+    )
+}
+export default PortfolioBody;
