a5c24257e455194d9d72f5361fb0c4d4d38631c4
DanielBlomberg1 <daniel.blombergof@gmail.com>
2022-02-12 12:47:53 +0200

    fix link in body

--- client/src/components/Portfolio/PortfolioBody.js
+++ client/src/components/Portfolio/PortfolioBody.js
@@ -9,7 +9,7 @@ import php from 'programming-languages-logos/src/php/php.svg';
 const PortfolioBody = () =>{
     const d = new Date();
     const years = d.getFullYear() - 2001;
-
+    
     return(
         <Fragment>
             <section className="relative">
@@ -17,7 +17,7 @@ const PortfolioBody = () =>{
                     <div className="flex flex-1 flex-col items-center lg:items-start">
                         <h2 className="text-black font-bold text-4xl text-center lg:text-left mb-6">This is my life</h2>
                         <p className="text-slate-600 text-2xl text-left mb-6"> I am {years} years of age. And software development is my passion</p>
-                        <button className="shadows-md btn btn-primary bg-violet-500 hover:bg-violet-400 border-0"> Check out my github</button>
+                        <a target="_blank" href="https://github.com/DanielBlomberg1" rel="noopener noreferrer" className="btn bg-violet-500 text-white hover:bg-violet-400">  Check out my Github</a>
                     </div>
                     <div
                         className="hidden md:block overflow-hidden bg-violet-400 rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg:-bottom-28 lg:-right-36"
